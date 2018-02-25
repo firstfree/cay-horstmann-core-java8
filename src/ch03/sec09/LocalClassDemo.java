a9c607038a1e6648b2158bb9b989c60e1e559d66
JesusOh <ojsboy@hotmail.com>
2018-02-25 22:38:34 +0900

    chapter3 sec09

---
+++ src/ch03/sec09/LocalClassDemo.java
@@ -0,0 +1,31 @@
+package ch03.sec09;
+
+import java.util.Random;
+
+public class LocalClassDemo {
+
+	private static Random generator = new Random();
+	
+	public static IntSequence randomInts(int low, int high) {
+		class RandomSequence implements IntSequence {
+			@Override
+			public boolean hasNext() {
+				return true;
+			}
+
+			@Override
+			public int next() {
+				return low + generator.nextInt(high - low + 1);
+			}
+		}
+		
+		return new RandomSequence();
+	}
+	
+	public static void main(String[] args) {
+		IntSequence dieTosses = randomInts(1, 6);
+		for (int i = 0; i < 10; ++i) {
+			System.out.println(dieTosses.next());
+		}
+	}
+}
