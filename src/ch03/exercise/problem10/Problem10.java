f774ab9c2646d0a3bb5fde4a587447146b6bc091
JesusOh <ojsboy@hotmail.com>
2018-03-04 06:44:44 +0900

    chapter3 exercise problem10 fixed

--- src/ch03/exercise/problem10/Problem10.java
+++ src/ch03/exercise/problem10/Problem10.java
@@ -1,42 +1,32 @@
 package ch03.exercise.problem10;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileFilter;
+import java.util.Arrays;
 
 public class Problem10 {
 
-	public static FileSequence getFileSequence(File[] files) {
-		return new FileSequence() {
-			int position;
-			
-			@Override
-			public File next() {
-				return files[position++];
-			}
-			
-			@Override
-			public boolean hasNext() {
-				return files.length > position;
-			}
-		};
+	public static File[] getSubDirectoriesLamda(File parentDir) {
+		return parentDir.listFiles((f) -> f.isDirectory());
 	}
 	
-	public static void getSubDirectories(File parentDir, List<File> subDirs) {
-		File[] dirs = parentDir.listFiles(File::isDirectory);
-		FileSequence fileSequence = getFileSequence(dirs);
-		while (fileSequence.hasNext()) {
-			File subDir = fileSequence.next();
-			subDirs.add(subDir);
-			getSubDirectories(subDir, subDirs);
-		}
+	public static File[] getSubDirectoriesMethod(File parentDir) {
+		return parentDir.listFiles(File::isDirectory);
+	}
+	
+	public static File[] getSubDirectoriesInnerClass(File parentDir) {
+		return parentDir.listFiles(new FileFilter() {
+			@Override
+			public boolean accept(File pathname) {
+				return pathname.isDirectory();
+			}
+		});
 	}
 	
 	public static void main(String[] args) {
 		File rootDir = new File(args[0]);
-		List<File> subDirs = new ArrayList<>();
-		getSubDirectories(rootDir, subDirs);
-		
-		System.out.println(subDirs);
+		System.out.println(Arrays.toString(getSubDirectoriesLamda(rootDir)));
+		System.out.println(Arrays.toString(getSubDirectoriesMethod(rootDir)));
+		System.out.println(Arrays.toString(getSubDirectoriesInnerClass(rootDir)));
 	}
 }
