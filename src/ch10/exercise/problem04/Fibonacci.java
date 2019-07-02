8cdedaa99b2f7bc3394cb2de97a2286a643523f9
firstfree <ojsboy@hotmail.com>
2019-07-02 23:43:45 +0900

    Add problem04 fibonacci

---
+++ src/ch10/exercise/problem04/Fibonacci.java
@@ -0,0 +1,36 @@
+package ch10.exercise.problem04;
+
+import java.util.Arrays;
+
+public class Fibonacci {
+
+  public static void main(String[] args) {
+    Matrix[] matrices = new Matrix[40];
+    Arrays.parallelSetAll(matrices, i -> new Matrix(new int[][]{{1, 1}, {1, 0}}));
+    Arrays.parallelPrefix(matrices, Matrix::multiply);
+
+    System.out.println(Arrays.toString(matrices));
+  }
+
+  public static class Matrix {
+
+    private int[][] f;
+
+    public Matrix(int[][] f) {
+      this.f = f;
+    }
+
+    public Matrix multiply(Matrix matrix) {
+      int x1 = f[0][0] * matrix.f[0][0] + f[0][1] * matrix.f[1][0];
+      int x2 = f[0][0] * matrix.f[0][1] + f[0][1] * matrix.f[1][1];
+      int y1 = f[1][0] * matrix.f[0][0] + f[1][1] * matrix.f[1][0];
+      int y2 = f[1][0] * matrix.f[0][1] + f[1][1] * matrix.f[1][1];
+      return new Matrix(new int[][]{{x1, x2}, {y1, y2}});
+    }
+
+    @Override
+    public String toString() {
+      return f[0][0] + "";
+    }
+  }
+}
